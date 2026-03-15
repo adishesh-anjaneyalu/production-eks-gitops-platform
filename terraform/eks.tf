8b2ed9475c3dc6a98d81f33507f1eca1f6008f03
laxmikantag <laxmikantamlucky@gmail.com>
2026-03-15 09:15:39 +0000

    removed ebs csi driver

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -35,9 +35,6 @@ module "eks" {
       before_compute = true
     }
     kube-proxy = {}
-    aws-ebs-csi-driver = {
-      befoew_compute = true
-    }
     vpc-cni = {
       before_compute = true
     }
